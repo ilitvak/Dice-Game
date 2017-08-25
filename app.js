ff7ae4b679b98c94ce4c0c624916bd94ab73dc9d
Irwin <irwin.litvak@gmail.com>
2017-08-25 17:11:50 -0400

    figured out functionality for switching of users utilziing ternary operator. also - moved some variables locally

--- app.js
+++ app.js
@@ -10,37 +10,39 @@ GAME RULES:
 */
 
 
-var scores, roundScore, activePlayer, dice, imgNumber;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
-//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
-
-var x = document.querySelector("#score-0").textContent;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".dice").style.display = "none";
 
 document.querySelector(".btn-roll").addEventListener("click", function(){
     
-    document.querySelector(".dice").style.display = "block";
+    var grabPlayer = document.querySelector("#current-" + activePlayer);
     
-    dice = Math.floor(Math.random() * 6 ) + 1;
+    var dice = Math.floor(Math.random() * 6 ) + 1;
     
-    imgNumber = dice;
+    var diceDOM = document.querySelector(".dice");
     
-    document.querySelector(".dice").src = "dice-" + imgNumber + ".png";
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+    console.log(grabPlayer);
+    if(dice !== 1) {
+        roundScore += dice;
+        grabPlayer.textContent = roundScore;
+    }
     
-  
-    
-//    roundScore += dice;
-//    
-//    if(dice === 1) {
-//        roundScore = 0;
-//    }
-    
+    else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+    }
     
 });
    
